a222b9d66192317e3c071872a10463b98e6ae2a8
cham-s <cattouma@student.42.fr>
2016-05-13 01:16:02 +0200

    fixed turn left

--- source/move_events.c
+++ source/move_events.c
@@ -1,9 +1,5 @@
 #include "wolf3d.h"
 
-//
-#define MAP_W 24
-#define MAP_H 24
-
 void	move_forward(t_map_info *mi, t_time_info *ti,  int world_map[MAP_H][MAP_W])
 {
 	if (world_map[(int)(mi->pos_x + mi->dir_x * ti->move_speed)][(int)mi->pos_y] == 0)
@@ -42,6 +38,6 @@ void	turn_left(t_map_info *mi, t_time_info *ti)
 	old_plane_x = mi->plane_x;
 	mi->dir_x = mi->dir_x * cos(ti->rot_speed) - mi->dir_y * sin(ti->rot_speed);
 	mi->dir_y = old_dir_x * sin(ti->rot_speed) + mi->dir_y * cos(ti->rot_speed); 
-	mi->plane_x = mi->plane_x * cos(-ti->rot_speed) - mi->plane_y * sin(ti->rot_speed);
-	mi->plane_y = old_plane_x * sin(-ti->rot_speed) + mi->plane_y * cos(ti->rot_speed); 
+	mi->plane_x = mi->plane_x * cos(ti->rot_speed) - mi->plane_y * sin(ti->rot_speed);
+	mi->plane_y = old_plane_x * sin(ti->rot_speed) + mi->plane_y * cos(ti->rot_speed); 
 }
