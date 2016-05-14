7ed8daf423d3592c94b100cfc929618bf15eef8e
cham-s <cattouma@student.42.fr>
2016-05-14 04:34:14 +0200

    constant for speed added

--- source/move_events.c
+++ source/move_events.c
@@ -1,23 +1,26 @@
 #include "wolf3d.h"
 
+//
+#define C_SPEED 0.0995
+
 void	move_forward(t_map_info *mi, t_time_info *ti,  int world_map[MAP_H][MAP_W])
 {
 	if (world_map[(int)(mi->pos_x + mi->dir_x * ti->move_speed)][(int)mi->pos_y] == 0)
 	{
-		mi->pos_x += mi->dir_x * 0.0888;
+		mi->pos_x += mi->dir_x * C_SPEED;
 		//
 		printf("move speed: %lf\n", ti->move_speed);
 	}
 	if (world_map[(int)mi->pos_x][(int)(mi->pos_y + mi->dir_y * ti->move_speed)] == 0)
-		mi->pos_y += mi->dir_y * 0.0888;
+		mi->pos_y += mi->dir_y * C_SPEED;
 }
 
 void	move_backward(t_map_info *mi, t_time_info *ti,  int world_map[MAP_H][MAP_W])
 {
 	if (world_map[(int)(mi->pos_x - mi->dir_x * ti->move_speed)][(int)mi->pos_y] == 0)
-		mi->pos_x -= mi->dir_x * 0.0888;
+		mi->pos_x -= mi->dir_x * C_SPEED;
 	if (world_map[(int)mi->pos_x][(int)(mi->pos_y - mi->dir_y * ti->move_speed)] == 0)
-		mi->pos_y -= mi->dir_y * 0.0888;
+		mi->pos_y -= mi->dir_y * C_SPEED;
 }
 
 void	turn_right(t_map_info *mi, t_time_info *ti)
@@ -29,10 +32,10 @@ void	turn_right(t_map_info *mi, t_time_info *ti)
 	(void)ti;
 	old_dir_x = mi->dir_x;
 	old_plane_x = mi->plane_x;
-	mi->dir_x = mi->dir_x * cos(-0.0888) - mi->dir_y * sin(-0.0888);
-	mi->dir_y = old_dir_x * sin(-0.0888) + mi->dir_y * cos(-0.0888); 
-	mi->plane_x = mi->plane_x * cos(-0.0888) - mi->plane_y * sin(-0.0888);
-	mi->plane_y = old_plane_x * sin(-0.0888) + mi->plane_y * cos(-0.0888); 
+	mi->dir_x = mi->dir_x * cos(-C_SPEED) - mi->dir_y * sin(-C_SPEED);
+	mi->dir_y = old_dir_x * sin(-C_SPEED) + mi->dir_y * cos(-C_SPEED); 
+	mi->plane_x = mi->plane_x * cos(-C_SPEED) - mi->plane_y * sin(-C_SPEED);
+	mi->plane_y = old_plane_x * sin(-C_SPEED) + mi->plane_y * cos(-C_SPEED); 
 	//
 	printf("rotation speed: %lf\n", ti->move_speed);
 }
@@ -46,8 +49,8 @@ void	turn_left(t_map_info *mi, t_time_info *ti)
 	(void)ti;
 	old_dir_x = mi->dir_x;
 	old_plane_x = mi->plane_x;
-	mi->dir_x = mi->dir_x * cos(0.0888) - mi->dir_y * sin(0.0888);
-	mi->dir_y = old_dir_x * sin(0.0888) + mi->dir_y * cos(0.0888); 
-	mi->plane_x = mi->plane_x * cos(0.0888) - mi->plane_y * sin(0.0888);
-	mi->plane_y = old_plane_x * sin(0.0888) + mi->plane_y * cos(0.0888); 
+	mi->dir_x = mi->dir_x * cos(C_SPEED) - mi->dir_y * sin(C_SPEED);
+	mi->dir_y = old_dir_x * sin(C_SPEED) + mi->dir_y * cos(C_SPEED); 
+	mi->plane_x = mi->plane_x * cos(C_SPEED) - mi->plane_y * sin(C_SPEED);
+	mi->plane_y = old_plane_x * sin(C_SPEED) + mi->plane_y * cos(C_SPEED); 
 }
